c7424e5d89be3a5494f138b7c2b3435b68a19b4b
simransingh-birdeye <simran.singh@birdeye.com>
2023-01-11 15:00:38 +0530

    changes

--- index.mjs
+++ index.mjs
@@ -12,21 +12,21 @@ const corsOptions = {
     credentials:true,
 };
 
-app.get("/getPincode", function (req, res) {
+route.get("/getPincode", function (req, res) {
     console.log("calling api");
-    // https.get('https://api.ip2location.com/v2/?key=demo&format=json&package=WS25', (resp) => {
-    //     let data = '';
-    //     resp.on('data', (chunk) => {
-    //         data += chunk;
-    //     });
-    //     resp.on('end', () => {
-    //         console.log(JSON.parse(data));
-    //         res.send(JSON.parse(data));
-    //     });
+    https.get('https://api.ip2location.com/v2/?key=demo&format=json&package=WS25', (resp) => {
+        let data = '';
+        resp.on('data', (chunk) => {
+            data += chunk;
+        });
+        resp.on('end', () => {
+            console.log(JSON.parse(data));
+            res.send(JSON.parse(data));
+        });
 
-    // }).on("error", (err) => {
-    //     res.send(err.message);
-    // });
+    }).on("error", (err) => {
+        res.send(err.message);
+    });
 })
 
 app.use("/api", route);
